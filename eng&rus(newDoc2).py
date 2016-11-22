843b5c0b58d7f931a297243cecd0de7f56171b8d
MSSERG <msserg132@mail.ru>
2016-11-22 20:25:32 +0300

    Add files via upload

---
+++ eng&rus(newDoc2).py
@@ -0,0 +1,393 @@
+﻿#	MSSERG	22.11.2016
+
+#	English				Russian
+
+en_i="i";				ru_i="я";
+en_you1="you";			ru_you1="ты, вы";
+en_we="we";				ru_we="мы";
+en_they="they";			ru_they="они";
+en_he="he";				ru_he="он";
+en_she="she";			ru_she="она";
+en_it="it";				ru_it="это";
+
+en_me="me";				ru_me="меня, мне";
+en_you2="you";			ru_you2="тебе, тебя";
+en_us="us";				ru_us="нас, нам";
+en_them="them";			ru_them="их, им";
+en_him="him";			ru_him="его, ему";
+en_her1="her";			ru_her1="её, ей";
+en_its1="its";			ru_its1="этому";
+
+en_my="my";				ru_my="мой";
+en_your="your";			ru_your="твой";
+en_our="our";			ru_our="наш";
+en_their="their";		ru_their="их";
+en_his="his";			ru_his="его";
+en_her2="her";			ru_her2="её";
+en_its2="its";			ru_its2="этого";
+
+#	if input == otvet to the resuilt+=1
+
+print(" MSSERG | 22.11.2016\n")
+
+resuilt=0
+
+if input(" Translate, english? - yes and no:")=="yes":
+	
+	print(" *******\n * who *\n *******\n")
+	
+	if input(" i :")==ru_i:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" you :")==ru_you1:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" we :")==ru_we:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" they :")==ru_they:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" he :")==ru_he:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" she :")==ru_she:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" it :")==ru_it:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	print(" ************\n * who/what *\n ************\n")
+	
+	if input(" me :")==ru_me:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" you :")==ru_you2:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" us :")==ru_us:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" them :")==ru_them:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" him :")==ru_him:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" her :")==ru_her1:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" its :")==ru_its1:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	print(" ***************\n * whose/whose *\n ***************\n")
+	
+	if input(" my :")==ru_my:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" your :")==ru_your:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" our :")==ru_our:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" their :")==ru_their:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" his :")==ru_his:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" her :")==ru_her2:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" its :")==ru_its2:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	print(" По итогам теста, вы набрали",resuilt,"из 21-го возможного балла.")
+
+resuilt=0
+
+if input(" Translate, russian? - yes and no:")=="yes":
+	
+	print(" *******\n * кто *\n *******\n")
+	
+	if input(" я :")==en_i:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" ты, вы :")==en_you1:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" мы :")==en_we:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" они :")==en_they:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" он :")==en_he:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" она :")==en_she:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" это :")==en_it:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	print(" *************\n * кого/чего *\n *************\n")
+	
+	if input(" меня, мне :")==en_me:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" тебе, тебя :")==en_you2:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" нас, нам :")==en_us:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" их, им :")==en_them:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" его, ему :")==en_him:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" её, ей :")==en_her1:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" этому :")==en_its1:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	print(" ***********\n * чей/чьё *\n ***********\n")
+	
+	if input(" мой :")==en_my:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" твой :")==en_your:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" наш :")==en_our:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" их :")==en_their:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" его :")==en_his:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" её :")==en_her2:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	if input(" этого :")==en_its2:
+		print(" All right!\n")
+		resuilt+=1
+	
+	else:
+		print(" Don't right!\n\a")
+		
+	
+	print(" По итогам теста, вы набрали",resuilt,"из 21-го возможного балла.")
+	
+input(" pause")
