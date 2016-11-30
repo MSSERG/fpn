ed6bff93ba2fef45ea8f9b1e46d2ebc8520d12ca
MSSERG <msserg132@mail.ru>
2016-11-30 18:52:49 +0300

    Add files via upload

---
+++ #pr.py
@@ -0,0 +1,25 @@
+﻿# MSSERG | 30.11.2016
+
+a = ' а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ь ы ъ э ю я\n'; b = ''; i = 0;
+
+while i < 22:
+	
+	i+=1
+	
+	b += a
+
+f = open( 'text.txt', 'w' )
+
+f.write( b )
+
+f.close
+
+input( 'запись завершена' )
+
+f = open( 'text.txt', 'r' )
+
+print( f.read() )
+
+f.close
+
+input( 'чтение завершено' )
