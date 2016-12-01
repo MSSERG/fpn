4b210caadeb8f92a2898b362bc42788b5a40d4ab
MSSERG <msserg132@mail.ru>
2016-12-01 13:46:19 +0300

    Add files via upload

---
+++ #pr(v.1.0).py
@@ -0,0 +1,21 @@
+﻿# MSSERG | 1.12.2016
+
+a = ' а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ь ы ъ э ю я '; b = ':'; i = 0;
+
+f = open( 'text.txt', 'a' )
+
+while i < 10000000:
+	
+	i += 1
+	
+	f.write( a )
+	
+	f.write( b )
+	
+	f.write( str( i ) )
+	
+	f.write( '\n' )
+
+f.close()
+
+input('запись завершена')
