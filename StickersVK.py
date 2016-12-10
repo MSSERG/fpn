a3fe392a77b3799947dd9336fc6e35316cbffee8
MSSERG <msserg132@mail.ru>
2016-12-11 00:03:15 +0300

    Update StickersVK.py

--- StickersVK.py
+++ StickersVK.py
@@ -1,16 +1,16 @@
-﻿# MSSERG | 3.12.2016
+# MSSERG | 3.12.2016
 
-# http://vk.com/images/stickers/112/128.png
+# http://vk.com/images/stickers/1/128.png
 
 linkPart1 = '<img src="http://vk.com/images/stickers/'; linkPart2 = '/128.png" />\n'; i = 0;
 
 f = open( 'linkStickersVK.txt', 'a' )
 
-while i < 3669:
+while i < 3693:
 	
 	i += 1
 	
-	print( 'завершена', i, 'из 3669 строк')
+	print( 'завершена', i, 'из 3693 строк')
 	
 	f.write( linkPart1 )
 	
