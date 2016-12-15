b5f2875a02b0454467808a4fe27758f772570961
MSSERG <msserg132@mail.ru>
2016-12-15 17:21:33 +0300

    Add files via upload

---
+++ vk-stickers-downloader-v.1.0.py
@@ -0,0 +1,67 @@
+﻿# MSSERG | 15.12.2016
+
+# https://vk.com/images/stickers/1/64.png
+# .........................................
+# https://vk.com/images/stickers/3717/512.png
+
+import urllib.request
+
+i = 0; k = 32; counter = 0; unique = 0;
+
+file = open( 'imageURL.html', 'w' )
+
+file.close()
+
+while i < 3717:
+	
+	i += 1; k = 32;
+	
+	file = open( 'imageURL.html', 'a' )
+	
+	file.write( '\n<hr />' )
+	
+	file.close()
+	
+	while k < 512:
+		
+		k *= 2; unique += 1;
+		
+		try:
+			
+			imageURL = '\n<img src="' + str( i ) + '~' + str( k ) + '.png" />'
+			
+			file = open( 'imageURL.html', 'a' )
+			
+			file.write( imageURL )
+			
+			file.close()
+			
+			#imageURL = 'https://vk.com/images/stickers/' + str( i ) + '/' + str( k ) + '.png'
+			
+			#imageName = str( i ) + '~' + str( k ) + '.png'
+			
+			#imageDownload = urllib.request.urlopen( imageURL ).read()
+			
+			#img = open( imageName, 'wb' )
+			
+			#img.write( imageDownload )
+			
+			#img.close()
+			
+			#print( 'Download image:', imageName )
+			
+		except IOError as qwerty:
+			
+			#print( 'Download failed image:', imageName )
+			
+			counter += 1
+			
+			continue
+
+print( 'All    imageURL =', unique )
+
+unique /= 4; unique -= counter;
+
+print( 'Unique imageURL =', int(unique) )
+
+input( '\nFinish write' )
