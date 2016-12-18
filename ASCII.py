2d322356c9edabf8fe95b11bcecd414f269c80e6
MSSERG <msserg132@mail.ru>
2016-12-18 18:18:40 +0300

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,15 @@
+﻿print( '\n  Перевод символов в ASCII код.\n' )
+
+while True:
+	
+	try:
+		
+		text = input( '  Введите любое значение:' )
+		
+		for l in text:
+			
+			print( ' ', l, '=', ord( l ), '\a' )
+	
+	except IOError as qwerty:
+		
+		text = '0'
